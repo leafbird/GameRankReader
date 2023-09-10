0580fa4890b8415d074603a849c042c226bd43a0
leafbird <florist.sk@gmail.com>
2023-09-10 21:18:34 +0900

    dev: data crawling - retrieve individual elements.

--- GameRankReader/Program.cs
+++ GameRankReader/Program.cs
@@ -16,19 +16,44 @@ class Program
         chromeOptions.AddArgument("--disable-infobars");
         IWebDriver driver = new ChromeDriver(chromeDriverService, chromeOptions);
 
-        ReadGooglePlayRanking(driver);
+        ReadSimilarWeb(driver);
     }
-
-    private static void ReadGooglePlayRanking(IWebDriver driver)
+    private static void ReadSimilarWeb(IWebDriver driver)
     {
-        // 웹 페이지로 이동
-        var targetUrl = "https://play.google.com/store/games";
+        var targetUrl = $"https://www.similarweb.com/apps/top/google/store-rank/kr/games/top-grossing/";
+        driver.Navigate().GoToUrl(targetUrl);
+
+        var selector = "body > div.wrapperBody--topRanking.wrapper-body.js-wrapperBody > main > div > div.topAppsSection.js-leaderBoard.js-appsSection > div.topAppsGrid > div > table > tbody";
+        IWebElement element = driver.FindElement(By.CssSelector(selector));
+
+        foreach (var child in element.FindElements(By.CssSelector("tr")))
+        {
+            // find image url
+            var imgElement = child.FindElement(By.XPath("td[2]/div/a/img"));
+            var imgUrl = imgElement.GetAttribute("src");
+
+            // find game title
+            var titleElement = child.FindElement(By.XPath("td[3]/div/a[1]/span"));
+            var titleText = titleElement.Text;
+            
+            // find game title
+            var companyElement = child.FindElement(By.XPath("td[4]/span"));
+            var companyText = companyElement.Text;
+            
+            Console.WriteLine($"{titleText} ({companyText})");
+        }
+   }
+
+
+    private static void ReadSensorTower(IWebDriver driver)
+    {
+        var targetDateStr = DateTime.Now.ToString("yyyy-MM-dd");
+        var targetUrl = $"https://app.sensortower.com/top-charts?category=game&country=KR&date={targetDateStr}&device=iphone&os=android";
         driver.Navigate().GoToUrl(targetUrl);
 
         // Shadow DOM 내부 요소에 액세스하는 방법 (예시)
-        var selector = "#yDmH0d > c-wiz.SSPGKf.glB9Ve > div > div > div.N4FjMb.Z97G4e > c-wiz > div > c-wiz > c-wiz:nth-child(2) > c-wiz > section > div > div:nth-child(3) > div > div > div";
+        var selector = "#mainContent > div.MuiBox-root.css-i9gxme > div > div.infinite-scroll-component__outerdiv > div > div.MuiTableContainer-root.css-kge0eu > table > tbody";
         IWebElement element = driver.FindElement(By.CssSelector(selector));
-        //IWebElement shadowHost = driver.FindElement(By.XPath("//*[@id='yDmH0d']/c-wiz[2]/div/div/div[1]/c-wiz/div/c-wiz/c-wiz[2]/c-wiz/section/div/div[3]/div/div/div/div[1]/div[1]/div[1]/div/a/div[1]/div")
 
         // 크롤링 작업 수행 (예시)
         string rankText = element.Text;
@@ -37,7 +62,7 @@ class Program
         Console.WriteLine(rankText);
     }
 
-    private static void ReadMobileIndexRanking(IWebDriver driver)
+    private static void ReadMobileIndex(IWebDriver driver)
     {
         // 웹 페이지로 이동
         var targetUrl = "https://www.mobileindex.com/mi-chart/realtime-rank";
