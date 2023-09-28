4aba8bbce8961056bee00370ff543789b74eef3c
leafbird <florist.sk@gmail.com>
2023-09-28 10:01:06 +0900

    fix: json 저장에서 '&'이 아직도 escape 당하고 있던 현상 수정.

--- GameRank.Core/Configs/JsonOption.cs
+++ GameRank.Core/Configs/JsonOption.cs
@@ -2,7 +2,6 @@
 
 using System.Text.Encodings.Web;
 using System.Text.Json;
-using System.Text.Unicode;
 
 public static class JsonOption
 {
@@ -12,7 +11,9 @@ public static class JsonOption
     {
         Default = new JsonSerializerOptions
         {
-            Encoder = JavaScriptEncoder.Create(UnicodeRanges.All), // 모든 코드에 escape 처리를 제거.
+            // Encoder = JavaScriptEncoder.Create(UnicodeRanges.All), // 모든 코드에 escape 처리를 제거.
+            // https://stackoverflow.com/questions/58003293/dotnet-core-system-text-json-unescape-unicode-string
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
             WriteIndented = true, // 파일에 저장할 때, 들여쓰기를 해서 저장하도록 설정
         };
     }
